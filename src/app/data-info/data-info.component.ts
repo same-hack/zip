54dd8ec515036e12cd580d55a206549f1fa17d2b
same-hack <samehack.info@gmail.com>
2023-07-12 15:24:05 +0900

    ad

--- src/app/data-info/data-info.component.ts
+++ src/app/data-info/data-info.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
 import { FileService } from '../file.service';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { files } from './example-data';
+
+/** File node data with possible child nodes. */
+export interface FileNode {
+  name: string;
+  type: string;
+  children?: FileNode[];
+}
+
+/**
+ * Flattened tree node that has been created from a FileNode through the flattener. Flattened
+ * nodes include level index and whether they can be expanded or not.
+ */
+export interface FlatTreeNode {
+  name: string;
+  type: string;
+  level: number;
+  expandable: boolean;
+}
 
 @Component({
   selector: 'app-data-info',
@@ -9,11 +33,36 @@ import { FileService } from '../file.service';
 export class DataInfoComponent {
   directories: any = [];
 
+  /** The TreeControl controls the expand/collapse state of tree nodes.  */
+  treeControl: FlatTreeControl<FlatTreeNode>;
+
+  /** The TreeFlattener is used to generate the flat list of items from hierarchical data. */
+  treeFlattener: MatTreeFlattener<FileNode, FlatTreeNode>;
+
+  /** The MatTreeFlatDataSource connects the control and flattener to provide data. */
+  dataSource: MatTreeFlatDataSource<FileNode, FlatTreeNode>;
+
   constructor(public service: FileService) {
+    // アップロードされたファイルの購読
     this.service.directories$.subscribe((res) => {
       console.log('res', res);
       this.directories = res;
     });
+
+    // ---------------------以下tree-----------------------
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren
+    );
+
+    this.treeControl = new FlatTreeControl(this.getLevel, this.isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(
+      this.treeControl,
+      this.treeFlattener
+    );
+    this.dataSource.data = files;
   }
 
   toBeforePage() {
@@ -24,5 +73,46 @@ export class DataInfoComponent {
     console.log('this.directories', this.directories);
     this.service.setDirectories(this.directories);
     this.service.showResult();
+  }
+
+  // ---------------------以下tree-----------------------
+  selectFile(node: any) {
+    const fileName = node.name;
+    // ファイル名を取得して利用する処理を実装
+
+    if (this.treeControl.isExpanded(node)) {
+      this.treeControl.collapse(node); // ツリーを閉じる
+    }
+    console.log(node);
+  }
+
+  /** Transform the data to something the tree can read. */
+  transformer(node: FileNode, level: number): FlatTreeNode {
+    return {
+      name: node.name,
+      type: node.type,
+      level,
+      expandable: !!node.children,
+    };
+  }
+
+  /** Get the level of the node */
+  getLevel(node: FlatTreeNode): number {
+    return node.level;
+  }
+
+  /** Get whether the node is expanded or not. */
+  isExpandable(node: FlatTreeNode): boolean {
+    return node.expandable;
+  }
+
+  /** Get whether the node has children or not. */
+  hasChild(index: number, node: FlatTreeNode): boolean {
+    return node.expandable;
+  }
+
+  /** Get the children for the node. */
+  getChildren(node: FileNode): FileNode[] | null | undefined {
+    return node.children;
   }
 }
